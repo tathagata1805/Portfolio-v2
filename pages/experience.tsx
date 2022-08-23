44359bcce6873af47a8b749a0ec29602d519f8b2
Tathagata <tathagata1805@gmail.com>
2022-08-23 21:42:15 +0530

    work updates and resume updates

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import Layout from '@/components/Layout';
 import InternshipCard from '@/components/InternshipCard';
-import MentorshipCard from '@/components/MentorshipCard';
 import VolunteerCard from '@/components/VolunteerCard';
 import Head from 'next/head';
 
 export default function skillsAndExperience() {
   const internData = {
-    quordnet: {
-      title: 'Quordnet Academy',
-      date: 'January 2022 – Present',
+    codeclouds: {
+      title: 'CodeClouds, Frontend Developer',
+      date: 'August 2022 – Present',
       place: 'Kolkata, India',
-      desc:
-        'Quordnet Academy is an Ed-Tech based startup where I developed the Frontend of the main website of Quordnet Services using React JS, Redux and Material UI and deployed the website on the internet. I also helped in designing the UI using design tools. I have also worked on building the Quordnet Academy website which is a one- stop education solution using React JS and other frontend technologies. The website contains features like in- built IDE, Blog section as well as Online portal for conducting video lectures.  I have worked in a team with skilled developers and designers and met all the deadlines successfully.',
+      desc: 'CodeClouds is a Multi National Company where I currently work as a Frontend Developer, in the Kolkata, India team. My responsibilities revolve around building the frontend of multiple client websites and further optimizing them and improving the code structures. I closely work with a team and my mentors to provide the most optimal solution to the clients. I work in tech stacks that include React JS, Redux, JavaScript, HTML, CSS, Bootstrap, MaterialUI, and other frontend technologies.',
+    },
+    quordnet: {
+      title: 'Quordnet Academy, Frontend Engineer (Intern)',
+      date: 'January 2022 – July 2022',
+      place: 'Kolkata, India',
+      desc: 'Quordnet Academy is an Ed-Tech based startup where I developed the Frontend of the main website of Quordnet Services using React JS, HTML, CSS and Material UI and deployed the website on the internet. I also helped in designing the UI using design tools. I have also worked on building the Quordnet Academy website which is a one- stop education solution using React JS and other frontend technologies. The website contains features like in- built IDE, Blog section as well as Online portal for conducting video lectures.  I have worked in a team with skilled developers and designers and met all the deadlines successfully.',
+    },
+    webprism: {
+      title: 'Web Prism IT Solutions PVT. Ltd, Frontend Engineer (Part Time)',
+      date: 'June 2022 – Present',
+      place: 'Kolkata, India',
+      desc: 'Web Prism IT Solutions Pvt. Ltd. is a service based startup where I work as a Freelance Frontend Engineer, responsible for developing and designing websites for our clients by collaborating with senior developers snd designers who come from different tech stacks and experiences. I solely handle the frontend domain and generally use HTML, CSS, Bootstrap, JavaScript, React to build the websites and also optimize the performances and work on existing codebases to improve their structure and scale them up.',
     },
   };
 
   const volData = {
     ureckon: {
       title: 'Ureckon, UEMK',
-      desig: 'Organizing Committee Member',
+      desig: 'Core Committee Member',
       year: '2019 - 2022',
       place: 'Kolkata, India',
     },
@@ -49,12 +59,27 @@ export default function skillsAndExperience() {
         <title>Expereience | GitHub</title>
       </Head>
       <Layout border="border-2">
-        <h3 className="text-lg font-semibold mt-3">Internships</h3>
+        <h3 className="text-lg font-semibold mt-3">Work Experiences</h3>
+        <InternshipCard
+          title={internData.codeclouds.title}
+          date={internData.codeclouds.date}
+          place={internData.codeclouds.place}
+          desc={internData.codeclouds.desc}
+        />
+
         <InternshipCard
           title={internData.quordnet.title}
           date={internData.quordnet.date}
           place={internData.quordnet.place}
           desc={internData.quordnet.desc}
+        />
+
+        <h3 className="text-lg font-semibold mt-3">Freelancing Works</h3>
+        <InternshipCard
+          title={internData.webprism.title}
+          date={internData.webprism.date}
+          place={internData.webprism.place}
+          desc={internData.webprism.desc}
         />
 
         <h3 className="text-lg font-semibold mt-3">Volunteer Experience</h3>
@@ -71,16 +96,16 @@ export default function skillsAndExperience() {
           place={volData.uemmun.place}
         />
         <VolunteerCard
-        title={volData.dev.title}
-        desig={volData.dev.desig}
-        year={volData.dev.year}
-        place={volData.dev.place}
+          title={volData.dev.title}
+          desig={volData.dev.desig}
+          year={volData.dev.year}
+          place={volData.dev.place}
         />
         <VolunteerCard
-        title={volData.cr.title}
-        desig={volData.cr.desig}
-        year={volData.cr.year}
-        place={volData.cr.place}
+          title={volData.cr.title}
+          desig={volData.cr.desig}
+          year={volData.cr.year}
+          place={volData.cr.place}
         />
       </Layout>
     </>
